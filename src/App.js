f5287e0e1ec880c0b673feccf7bf172ebfcf6791
joseg-alvesg <cinhoalves85@gmail.com>
2023-01-11 15:13:36 -0300

    inicio do projeto

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
+        <span>requisito 01 inicio do projeto</span>
         <p>Edit src/App.js and save to reload.</p>
         <a
           className="App-link"
