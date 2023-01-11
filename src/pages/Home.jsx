31fe3e3cbc50fa1e650ffc3ac4875262e3e0f0b8
joblanco <jo_blanco9@yahoo.com.br>
2023-01-11 16:30:38 -0300

    Requisito 2 concluído, criado input e adcionado router ao app.js

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Input from '../components/Input';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Input />
+        <span data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </span>
+      </div>
+    );
+  }
+}
