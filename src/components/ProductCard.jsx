6418dea30790cab619b85d2f2b76d24efd935782
joblanco <jo_blanco9@yahoo.com.br>
2023-01-12 20:23:50 -0300

    Requisito 8 Concluído

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -45,7 +45,7 @@ export default class ProductCard extends Component {
 export default class ProductCard extends Component {
   render() {
     const { title, thumbnail, price, id } = this.props;
-    console.log(id);
+    // console.log(id);
     return (
       <div data-testid="product">
         <Link to={ `/product/${id}` } data-testid="product-detail-link">
