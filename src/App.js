5d4d205bd0fc45c8cb2203311b0352642f189184
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-01-13 19:56:13 -0300

    12.Chekout, preenchimento dos  dados e escolhendo a forma de pagamento

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Checkout from './pages/Checkout';
-import { Home } from './pages/Home';
-import { Product } from './pages/Product';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 // import { getCategories, getProductsFromCategoryAndQuery } from './services//api';
 function App() {
